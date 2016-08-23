daad4aa8324c36b53b110b4310fa69429d120542
Wilson Ding <shodai100@gmail.com>
2016-08-23 18:26:22 -0500

    Added symlinking

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 echo "Setting up your Mac..."
 
+# Moving dotfiles into ~
+files="bash_profile gitconfig"
+dir=~/Documents/dotfiles
+
+for file in $files; do
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
