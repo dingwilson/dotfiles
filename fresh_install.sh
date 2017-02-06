1e1f3ea20da07d44933c9a65fe392c890226b5b4
Wilson Ding <shodai100@gmail.com>
2017-02-06 08:39:30 -0600

    Added fresh_install.sh which includes ssh-keygen and addition to GitHub account.

---
+++ fresh_install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+echo "Setting up your Mac..."
+
+# XCode Command Line Tools
+
+if ! xcode-select --print-path &> /dev/null; then
+
+  # Prompt user to install the XCode Command Line Tools
+  xcode-select --install &> /dev/null
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # Wait until the XCode Command Line Tools are installed
+  until xcode-select --print-path &> /dev/null; do
+    sleep 5
+  done
+
+  print_result $? 'Install XCode Command Line Tools'
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # Point the `xcode-select` developer directory to
+  # the appropriate directory from within `Xcode.app`
+  # https://github.com/alrra/dotfiles/issues/13
+
+  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+  print_result $? 'Make "xcode-select" developer directory point to Xcode'
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # Prompt user to agree to the terms of the Xcode license
+  # https://github.com/alrra/dotfiles/issues/10
+
+  sudo xcodebuild -license
+  print_result $? 'Agree with the XCode Command Line Tools licence'
+
+fi
+
+# Setup SSH
+pub=$HOME/.ssh/id_rsa.pub
+echo 'Checking for SSH key, generating one if it does not exist...'
+  [[ -f $pub ]] || ssh-keygen -t rsa
+
+echo 'Copying public key to clipboard. Paste it into your Github account...'
+  [[ -f $pub ]] && cat $pub | pbcopy
+  open 'https://github.com/account/ssh'
+
+# Moving dotfiles into ~
+files="bash_profile zshrc vimrc gitconfig aliases gitignore"
+dir=~/dotfiles
+
+for file in $files; do
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/dotfiles/$file ~/.$file
+done
+
+# Set ~/.gitignore as global .gitignore
+git config --global core.excludesfile ~/.gitignore
+echo "Set up your global .gitignore at ~/.gitignore"
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Installed Homebrew"
+fi
+
+# Turn off Homebrew Analytics
+brew analytics off
+
+# Update Homebrew recipes
+echo "Updating Homebrew."
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+echo "Tapping Homebrew Bundle"
+brew tap homebrew/bundle
+echo "Brewing bundle."
+brew bundle
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Updating all ruby gems
+ech "Updating all gems.."
+sudo gem update
+
+# Check for Cocoapods and install if we don't have it
+if test ! $(which pod); then
+  echo "Installing Cocoapods."
+  sudo gem install cocoapods
+fi
+
+# Setup Various MacOS preferences
+echo "Setting Up Various MacOS Preferences..."
+source macos/.macos
