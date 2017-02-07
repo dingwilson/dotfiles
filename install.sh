5e9c0f3ef12b94cb0e0462179d3531c12e5790e0
Wilson Ding <shodai100@gmail.com>
2017-02-07 13:29:34 -0600

    Modified system prompts for install.sh

--- install.sh
+++ install.sh
@@ -5,48 +5,48 @@ files="bash_profile zshrc vimrc gitconfig aliases gitignore"
 dir=~/dotfiles
 
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
+    echo "Creating symlink to $file in home directory..."
     ln -s $dir/dotfiles/$file ~/.$file
 done
 
 # Set ~/.gitignore as global .gitignore
+echo "Setting up your global .gitignore at ~/.gitignore..."
 git config --global core.excludesfile ~/.gitignore
-echo "Set up your global .gitignore at ~/.gitignore"
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo "Installed Homebrew"
+	echo "Installing Homebrew..."
+  	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Turn off Homebrew Analytics
-echo "Turning off Homebrew Analytics/Tracking"
+echo "Turning off Homebrew Analytics/Tracking..."
 brew analytics off
 
 # Update Homebrew recipes
-echo "Updating Homebrew."
+echo "Updating Homebrew..."
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-echo "Tapping Homebrew Bundle"
+echo "Tapping Homebrew Bundle..."
 brew tap homebrew/bundle
-echo "Brewing bundle."
+echo "Brewing bundle..."
 brew bundle
 
 # Install Oh-My-Zsh
-echo "Installing Oh-My-Zsh"
+echo "Installing Oh-My-Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Make ZSH the default shell environment
-echo "Switching to Zsh as Default Shell Environment"
+echo "Switching to Zsh as Default Shell Environment..."
 chsh -s $(which zsh)
 
 # Updating all ruby gems
-echo "Updating all gems.."
+echo "Updating all gems..."
 sudo gem update
 
 # Check for Cocoapods and install if we don't have it
 if test ! $(which pod); then
-  echo "Installing Cocoapods."
+  echo "Installing Cocoapods..."
   sudo gem install cocoapods
 fi
